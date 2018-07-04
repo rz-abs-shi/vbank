e42460cb7e9983b8fffcb4caff576ed5f91bd190
Reza Abbasi <rz.abs.shi@gmail.com>
2018-07-04 06:09:21 +0430

    bank configuration added to central bank

--- models/central_bank.py
+++ models/central_bank.py
@@ -5,3 +5,13 @@ from models import User
 class CentralBank(peewee.Model):
 
     manager = peewee.ForeignKeyField(User)
+
+    number_of_transactions_in_block = peewee.SmallIntegerField(default=0)
+    transaction_fee = peewee.FloatField(default=0)
+    block_miner_reward = peewee.FloatField(default=0)
+    difficulty = peewee.SmallIntegerField(default=0)
+    bank_balance_min_percent_to_load = peewee.FloatField(default=0)
+
+    def has_valid_configuration(self):
+        return self.number_of_transactions_in_block > 0 and self.transaction_fee > 0 and \
+               self.block_miner_reward > 0 and self.difficulty > 0 and self.bank_balance_min_percent_to_load > 0
