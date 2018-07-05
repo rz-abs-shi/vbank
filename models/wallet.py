e35bc542c76112dbb4d0a28a63bb2b5ef560fd0a
Reza Abbasi <rz.abs.shi@gmail.com>
2018-07-06 01:48:48 +0430

    encryption and decryption added to Wallet model

--- models/wallet.py
+++ models/wallet.py
@@ -1,19 +1,40 @@
 import peewee
+
+from crypto.aes import AESCipher
+from crypto.rsa import new_keys, import_key
 from models import BaseModel
-from crypto.rsa import new_keys
-
-
-def private_key_default():
-    public_key, private_key = new_keys(1024)
-    return private_key
 
 
 class Wallet(BaseModel):
 
-    private_key_encrypted = peewee.CharField(max_length=1024, default=private_key_default)
+    private_key_encrypted = peewee.CharField(max_length=2048)
 
     public_key = None
     private_key = None
 
     def decrypt(self, password):
-        pass
+        cipher = AESCipher(password)
+
+        try:
+            private_key_str = cipher.decrypt(self.private_key_encrypted)
+
+            if not private_key_str.startswith('-----BEGIN RSA PRIVATE KEY-----'):
+                raise Exception('Password was invalid to decode private key')
+
+            self.private_key = import_key(private_key_str)
+            self.public_key = self.private_key.publickey()
+
+        except UnicodeDecodeError:
+            raise Exception('Password was invalid to decode private key')
+
+    @staticmethod
+    def create_new_wallet(password):
+        wallet = Wallet()
+        public_key, private_key = new_keys(1024)
+        private_key_as_str = private_key.export_key().decode()
+
+        cipher = AESCipher(password)
+        wallet.private_key_encrypted = cipher.encrypt(private_key_as_str)
+        wallet.save()
+
+        return wallet
