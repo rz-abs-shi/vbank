551f9e0746d68eaac1217432a26cca7bc0093270
Reza Abbasi <rz.abs.shi@gmail.com>
2018-07-07 00:05:17 +0430

    unique constraint added to Bank model

--- models/bank.py
+++ models/bank.py
@@ -1,11 +1,12 @@
 import peewee
-from models import BaseModel, User, CentralBank, BankToken
+
+from models import BaseModel, User, BankToken
 
 
 class Bank(BaseModel):
 
     manager = peewee.ForeignKeyField(User, unique=True)
 
-    bank_name = peewee.CharField()
+    bank_name = peewee.CharField(unique=True)
 
     bank_token = peewee.ForeignKeyField(BankToken, unique=True)
